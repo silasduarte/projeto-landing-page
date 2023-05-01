4dc935073e5464b35a7b891d7cef9bd829d9ae63
Silas Duarte <silasduarte1@hotmail.com>
2023-05-01 00:47:04 -0300

    entrega landing page

---
+++ js.js
@@ -0,0 +1,9 @@
+function scrollInto(id) {
+    var elmnt = document.getElementById(id);
+    elmnt.scrollIntoView({
+      behavior: "smooth",
+      block: "start",
+      inline: "nearest"
+    });
+  }
+  
